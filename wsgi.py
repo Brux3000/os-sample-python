efbc145746c36426d31250c951f74ae197b1f55f
Brux3000 <brunello.bonanni@gmail.com>
2019-08-04 16:05:54 +0200

    inserted restfull.

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,33 @@
-from flask import Flask
+#!flask
+from flask import Flask, jsonify
+
 application = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy Groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
 
 @application.route("/")
 def hello():
     return "Hello World BRUNELLO!"
+
+
+@apppplication.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
 
 if __name__ == "__main__":
     application.run()
