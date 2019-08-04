404b25c1d84f51df9a8b20cfeff41d1605b5691d
Brux3000 <brunello.bonanni@gmail.com>
2019-08-04 16:42:59 +0200

    inserted restfull01.

--- wsgi.py
+++ wsgi.py
@@ -36,5 +36,24 @@ def get_task(task_id):
     return jsonify({'task': task[0]})
 
 
+@application.route('/newtask', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+
+@application.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
 if __name__ == "__main__":
     application.run()
