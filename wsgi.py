cace011cf97ba26ff076ce546ddfc9110fa369eb
Brux3000 <brunello.bonanni@gmail.com>
2019-08-05 15:47:10 +0200

    create post

--- wsgi.py
+++ wsgi.py
@@ -46,15 +46,19 @@ def masks_task():
 @application.route('/tasks', methods=['POST'])
 def create_task():
     if not request.json or not 'title' in request.json:
-        abort(400)
-    task = {
-        'id': tasks[-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description', ""),
-        'done': False
-    }
-    tasks.append(task)
-    return jsonify({'task': task})
+       return "Error in Jason"
+
+#        abort(400)
+#    task = {
+#        'id': tasks[-1]['id'] + 1,
+#        'title': request.json['title'],
+#        'description': request.json.get('description', ""),
+#        'done': False
+#    }
+#    tasks.append(task)
+#    return jsonify({'task': task})
+
+     return "End Post Procedure"
 
 @application.route('/tasks/<int:task_id>', methods=['PUT'])
 def update_task(task_id):
