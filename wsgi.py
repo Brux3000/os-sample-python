19e8b564809948364f7f7b3c63fc7b15b9aea807
Brux3000 <brunello.bonanni@gmail.com>
2019-08-09 22:53:32 +0200

    create post

--- wsgi.py
+++ wsgi.py
@@ -5,23 +5,6 @@ from flask import make_response
 from flask import request
 
 import mysql.connector
-from mysql.connector import errorcode
-
-try:
-  cnx = mysql.connector.connect(user='brunello', password='bonanni',
-                              host='127.0.0.1',
-                              database='sampledb')
-
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
-else:
-  cnx.close()
-
 
 application = Flask(__name__)
 
